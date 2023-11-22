3ca5b17aa3ba108bb35e5a6b9c5a9a11e9d09def
BathoBence <127421217+BathoBence@users.noreply.github.com>
2023-11-22 20:34:14 +0100

    fixed after merge-conflict

--- src/main/java/com/codecool/dungeoncrawl/data/actors/Actor.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Actor.java
@@ -28,6 +28,10 @@ public abstract class Actor implements Drawable {
         return health;
     }
 
+    public void increaseHealth(int bonusHealth){
+        health += bonusHealth;
+    }
+
     public Cell getCell() {
         return cell;
     }
